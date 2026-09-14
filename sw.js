c2718e12c2a24e9296c31f730f6d4b7a1062af49
김관현 <jamaica8612@gmail.com>
2026-09-15 01:17:17 +0900

    Prepare PWA Beta 1.08 install links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "quickflex-shell-v1.0.59-flexnote-beta-1.07-startup-1-usage-guide-1";
+const CACHE_NAME = "quickflex-shell-v1.0.60-flexnote-beta-1.08-startup-1-usage-guide-1";
 const SHELL_FILES = [
   "./",
   "./index.html",
@@ -65,7 +65,7 @@ const SHELL_FILES = [
   "./src/lib/revenue.js",
   "./src/lib/format.js",
   "./src/lib/work-date.js",
-  "./manifest.webmanifest?v=1.0.59",
+  "./manifest.webmanifest?v=1.0.60",
   "./icon-192.png?v=3",
   "./icon-512.png?v=3",
   "./icon-maskable-192.png?v=3",
